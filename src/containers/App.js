a18fc93de52f235b1684473c3c8f8ec8000e3a0b
wei30172 <claire610323@gmail.com>
2020-04-01 21:09:39 +0800

    add exclamation mark!

--- src/containers/App.js
+++ src/containers/App.js
@@ -33,7 +33,7 @@ class App extends Component {
 			<h1>Loading</h1> :
 			(
 				<div className='tc'>
-					<h1 className='f1'>RoboFriends</h1>
+					<h1 className='f1'>Hi RoboFriends !!!</h1>
 					<SearchBox SearchChange={this.onSearchChange}/>
 					<Scroll>
 						<ErrorBoundry>
